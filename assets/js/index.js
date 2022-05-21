c92c412c95c2590ec3f3efcddd09c44812ec3ed6
Wavelopper <alnetweb59@gmail.com>
2022-05-21 19:32:05 +0100

    [UPDATE] Ajout à la liste les élément, suppression et maj

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,97 +1,57 @@
-// On va chercher les différents élements
-let form = document.querySelector("form");
-let taskList = document.getElementById("taskList");
-let newTask = document.getElementById("newTask");
+import { saveInLocalstorage } from "./localStorage.js";
+// Récupérer les élements du DOM
+let newTask = document.querySelector("#new_task");
 let reset = document.getElementById("reset");
-let body = document.querySelector("body");
-// On initialise un compteur qui fait la taille du localStorage
-let compteur = localStorage.length;
+let taskList = document.querySelector("#task_list");
+let form = document.querySelector("form");
 
-/* ================ FORMULAIRE TODOLIST ================= */
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  //  On crée les nouveaux élément html
-  let list = document.createElement("li");
-  //  Récupérer la valeur entré par l'utilisateur
-  let newEntry = newTask.value;
-  if (newEntry != "") {
-    list.innerText = `Chose à faire : ${newEntry}`;
-    // On stocke le résultat dans le localStorage
-    saveLocalStorage(compteur++, newEntry);
-    taskList.appendChild(list);
+// On créer notre liste de tâche vide
+let list = [];
+
+// On ajoute un évenement au submit du bouton
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  addToDo(event.target.new_task.value);
+  newTask.value = null;
+});
+
+// On ajoute une tâche à notre liste
+const addToDo = (value) => {
+  if (value != "") {
+    list.push({ nom: value });
+    displayList({ nom: value });
+    saveInLocalstorage(list);
   } else {
-    alert("Veuillez entrer une tâche, ça suffit la procrastination. :)");
-  }
-  //   On vide notre formulaire après l'insertion
-  newTask.value = "";
-});
-
-//  On vide notre liste html et notre localStorage
-reset.addEventListener("click", (e) => {
-  e.preventDefault();
-  window.localStorage.clear();
-  taskList.innerHTML = "";
-});
-
-// On sauvegarde nos données dans le localStorage
-function saveLocalStorage(name, value){
-  window.localStorage.setItem(name, value);
-}
-
-// Récupère la liste dans le localStorage
-function getList(myList) {
-  for(let i = 0; i < compteur; i++){
-    let keyTask = window.localStorage.key(i);
-    myList = window.localStorage.getItem(keyTask);
-  }
-  return myList;
-}
-
-/* ================ FORMULAIRE COULEUR ================= */
-
-// On va chercher les différents élements du second formulaire
-let mySelectedColor = document.getElementById("color");
-let checkColor = document.getElementById("checkColor");
-let deleteSessionStorage = document.getElementById("delete");
-
-// function qui retourne ma couleur sauvegardé dans mon sessionStorage
-function getColor(newColor) {
-  if (!newColor) {
-    // On initialise une couleur si le sessionStorage est vide
-    newColor = "#F0F0F0";
-  } else {
-    //   Sinon on récupère la valeur stockéé
-    newColor = window.sessionStorage.getItem("myColor");
-    return newColor;
-  }
-}
-
-// On change le background du body après avoir choisi sa couleur
-mySelectedColor.addEventListener("change", () => {
-  newColor = mySelectedColor.value;
-  body.style.setProperty("background-color", newColor);
-  window.sessionStorage.setItem("myColor", newColor);
-});
-
-// On écoute notre évenement sur notre bouton de suprression du sessionStorage
-deleteSessionStorage.addEventListener("click", () => {
-  window.sessionStorage.clear();
-});
-// Au chargement de la page :
-window.onload = (event) => {
-  for(let i = 0; i < compteur; i++){
-    let myListSave = document.createElement('li');
-    let value = getList(compteur);
-    myListSave.innerText = `${value}`;
-    taskList.appendChild(myListSave);
-  } 
-  // récupère notre couleur stocké dans sessionStorage
-  let newColor = window.sessionStorage.getItem("myColor");
-  getColor(newColor);
-  //   On change la couleur si une couleur est sauvegardé dans le sessionStorage
-  body.style.setProperty("background-color", newColor);
-  //   On vérifie si notre checkbox est coché
-  if (checkColor.checked) {
-    window.sessionStorage.setItem("myColor", newColor);
+    alert("Il faut ajouter une tâche, ça suffit la procrastination ! :)");
   }
 };
+// On met à jour notre liste
+const updateList = () => {
+  list.forEach((value) => {
+    displayList(value);
+  });
+};
+
+// On affiche notre liste
+function displayList(value) {
+  let li = document.createElement("li");
+  li.innerText = value.nom;
+  taskList.appendChild(li);
+}
+
+// On vide notre liste 
+reset.addEventListener("click", (event)=>{
+  event.preventDefault();
+  list = [];
+  while(taskList.firstChild){
+    taskList.removeChild(taskList.firstChild);
+  }
+  window.localStorage.removeItem("todo");
+  updateList();
+})
+
+// Au chargement de la page
+window.addEventListener("load", () => {
+  list = JSON.parse(localStorage.getItem('todo')) || [];
+  updateList();
+});
