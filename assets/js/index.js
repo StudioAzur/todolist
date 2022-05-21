7758aa1be3c7e7796b376651c390dc1a00b86c81
Wavelopper <alnetweb59@gmail.com>
2022-05-21 20:40:18 +0100

    [UPDATE]

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,4 @@
-import { saveInLocalstorage } from "./localStorage.js";
+import { saveInLocalstorage } from "./save.js";
 // Récupérer les élements du DOM
 let newTask = document.querySelector("#new_task");
 let reset = document.getElementById("reset");
@@ -39,19 +39,58 @@ function displayList(value) {
   taskList.appendChild(li);
 }
 
-// On vide notre liste 
-reset.addEventListener("click", (event)=>{
+// On vide notre liste
+reset.addEventListener("click", (event) => {
   event.preventDefault();
   list = [];
-  while(taskList.firstChild){
+  while (taskList.firstChild) {
     taskList.removeChild(taskList.firstChild);
   }
   window.localStorage.removeItem("todo");
   updateList();
-})
+});
+
+// On identifie nos élements DOM
+let checkColor = document.querySelector("input[type=checkbox]");
+let inputColor = document.querySelector("#color");
+let emptySession = document.querySelector("#delete");
+// On initialise une couleur de base
+let colorInit = "#f0f0f0";
+// On modifie avec un sélécteur notre couleur de background
+const changeColor = (color) => {
+  document
+    .querySelector("body")
+    .setAttribute("style", `background-color:${color}`);
+};
+
+// On remplace notre couleur la checkbox est coché
+checkColor.addEventListener("change", () => {
+  if (checkColor.checked) {
+    sessionColor(color);
+  }
+});
+
+// On sauvegarde notre couleur pour la session en cours
+const sessionColor = (color) => {
+  sessionStorage.setItem("color", color.value);
+  console.log(`color saved :  ${color.value}`);
+};
+
+// On écoute le changement de couleur sur l'input
+inputColor.addEventListener("input", (event) => {
+  colorInit = event.target.value;
+  changeColor(colorInit);
+});
+
+// Au clique on vide les informations dans le sessionStorage
+emptySession.addEventListener("click", () => {
+  sessionStorage.removeItem("color");
+});
 
 // Au chargement de la page
 window.addEventListener("load", () => {
-  list = JSON.parse(localStorage.getItem('todo')) || [];
+  list = JSON.parse(localStorage.getItem("todo")) || [];
+  colorInit = sessionStorage.getItem("color") || "#ffffff";
+  changeColor(colorInit);
   updateList();
 });
